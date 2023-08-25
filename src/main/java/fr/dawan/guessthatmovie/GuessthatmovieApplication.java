2d932b337a9514babe572d59a7f2957a74c81d14
Alboran <97444137+ImAlboran@users.noreply.github.com>
2023-08-25 11:58:08 +0200

    Initial commit

---
+++ src/main/java/fr/dawan/guessthatmovie/GuessthatmovieApplication.java
@@ -0,0 +1,13 @@
+package fr.dawan.guessthatmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessthatmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessthatmovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/dawan/guessthatmovie/GuessthatmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dawan.guessthatmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessthatmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
